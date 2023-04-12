aab52ff1d4721f69e49240c31f1742e1fe85bfdf
LucasLib <128758795+LucasLib@users.noreply.github.com>
2023-04-12 19:35:43 -0300

    1

---
+++ ex01.py
@@ -0,0 +1,5 @@
+S = float(input('Digite a variação de espaço em quilômetros:'))
+T = float(input('Digite a variação de tempo em horas:'))
+Resultado = S / T
+
+print (f' Velocidade média dada a variação espacial e a variação temporal = {Resultado}')
---
+++ ex02.py
@@ -0,0 +1,4 @@
+A = int(input('Digite o valor de A: '))
+B = int(input('Digite o valor de B: '))
+Resultado = A-B
+print(f'A diferença de A e B é igual {Resultado}')
