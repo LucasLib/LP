15ffb9c0d1de57c2d4dd20c93497f79358038696
LucasLib <128758795+LucasLib@users.noreply.github.com>
2023-04-27 20:14:44 -0300

    Add files via upload

---
+++ ex09.py
@@ -0,0 +1,8 @@
+
+gasolina = float(input('Digite a quantidade de gasolina foi colocada: '))
+
+litros = float(input('Digite o preço do litro: '))
+# Processamento:
+resultado = gasolina/litros
+#Saída:
+print(f'O tanque foi abastecido com {resultado} litros')
